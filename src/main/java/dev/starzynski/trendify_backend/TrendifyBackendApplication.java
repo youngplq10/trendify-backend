f82809ed98549fac4300677efa5850ac26221a65
youngplq10 <patryk.plq@o2.pl>
2025-03-18 20:00:51 +0100

    Initial commit

---
+++ src/main/java/dev/starzynski/trendify_backend/TrendifyBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.starzynski.trendify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendifyBackendApplication.class, args);
+	}
+
+}
